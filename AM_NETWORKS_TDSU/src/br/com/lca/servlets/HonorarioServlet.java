2fecbf1607c42bb202f0d9058d516ba9011a7e18
gruponetworks <grupo_networks@outlook.com>
2014-10-30 20:38:56 -0200

    Alterações Servlet Honor

--- AM_NETWORKS_TDSU/src/br/com/lca/servlets/HonorarioServlet.java
+++ AM_NETWORKS_TDSU/src/br/com/lca/servlets/HonorarioServlet.java
@@ -1,12 +1,22 @@
 package br.com.lca.servlets;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import br.com.lca.beans.processo.Cliente;
+import br.com.lca.beans.processo.Periodo;
+import br.com.lca.beans.processo.Processo;
+import br.com.lca.bo.ProcessoBO;
+import br.com.lca.exception.LcaExpection;
 
 @WebServlet("/honorarioServlet")
 public class HonorarioServlet extends HttpServlet {
@@ -28,6 +38,7 @@ public class HonorarioServlet extends HttpServlet {
 		switch (request.getParameter("acao")) {
 
 		case "listarHonorario":
+			listarHonorario(request);
 			break;
 		case "lancarHonorario":
 			break;
@@ -46,4 +57,35 @@ public class HonorarioServlet extends HttpServlet {
 			excecao.printStackTrace();
 		}
 	}
+	
+	private void listarHonorario(HttpServletRequest request) {
+		try {
+			String numeroProcesso = request.getParameter("numeroDoProcesso");
+
+			ArrayList<Processo> retornoBusca = new ArrayList<Processo>();
+
+			if (isNotNullOrEmpty(numeroProcesso)) {
+
+				retornoBusca = ProcessoBO.getInstance()
+						.listarProcessoPorNumero(
+								new Processo(Integer.parseInt(numeroProcesso)));
+			} else {
+				request.setAttribute("mensagemErro",
+						"Informe um número de processo.");
+			}
+
+			request.setAttribute("listaHonorario", retornoBusca);
+
+		} catch (LcaExpection excecao) {
+			request.setAttribute("mensagemErro", excecao.getMessage());
+		} catch (NumberFormatException excecao) {
+			request.setAttribute("mensagemErro",
+					"Erro ao converter numérico, verifique os dados inseridos.");
+		}
+	}
+	
+	private boolean isNotNullOrEmpty(String string) {
+		return !(string.equals(null) || string.trim().isEmpty());
+	}
+
 }
